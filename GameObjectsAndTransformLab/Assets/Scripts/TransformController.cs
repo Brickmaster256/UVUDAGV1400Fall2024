bb56a626e137bfbc67a2cd53a57ebb4e955037a7
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-04 15:37:54 -0600

    commented the changes to prove I have completed the first assignment

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -7,11 +7,11 @@ public class TransformController : MonoBehaviour
     private void Update()
     {
         var y = Mathf.PingPong(Time.time, 3);
-        var x = Mathf.Cos(Time.time * 5);
-        var z = Mathf.Tan(Time.time * 2);
-        var p = new Vector3(z, y, x);
+        //var x = Mathf.Cos(Time.time * 5);
+        //var z = Mathf.Tan(Time.time * 2);
+        var p = new Vector3(0, y, 0);
         transform.position = p;
 
-        transform.Rotate( new Vector3(30, 30 * Time.deltaTime, 30) * Time.deltaTime);
+        transform.Rotate( new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
