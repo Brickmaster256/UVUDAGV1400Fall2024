22979169b46966804934a309cf5f23a521005ca9
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-04 15:35:36 -0600

    added code to allow the box to zip past the screen

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -6,10 +6,12 @@ public class TransformController : MonoBehaviour
 {
     private void Update()
     {
-        var x = Mathf.PingPong(Time.time, 1);
-        var p = new Vector3(0, x, 0);
+        var y = Mathf.PingPong(Time.time, 3);
+        var x = Mathf.Cos(Time.time * 5);
+        var z = Mathf.Tan(Time.time * 2);
+        var p = new Vector3(z, y, x);
         transform.position = p;
 
-        transform.Rotate( new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate( new Vector3(30, 30 * Time.deltaTime, 30) * Time.deltaTime);
     }
 }
