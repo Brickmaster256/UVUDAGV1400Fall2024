69901dc394aac64779659a2ee99c128e63e3e642
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-04 12:44:06 -0600

    made it so the box rotates as it moves up and down
    It now looks like a floating item from a video game like minecraft

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -9,5 +9,7 @@ public class TransformController : MonoBehaviour
         var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
         transform.position = p;
+
+        transform.Rotate( new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
