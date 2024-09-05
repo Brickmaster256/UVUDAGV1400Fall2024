b5db2eaab164a14684878f001b25a506fc0a6a9e
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-05 11:59:35 -0600

    added variables to change how to box moved

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -7,11 +7,11 @@ public class TransformController : MonoBehaviour
     private void Update()
     {
         var y = Mathf.PingPong(Time.time, 3);
-        //var x = Mathf.Cos(Time.time * 5);
-        //var z = Mathf.Tan(Time.time * 2);
-        var p = new Vector3(0, y, 0);
+        var z = Mathf.Cos(Time.time * 5);
+        var x = Mathf.Tan(Time.time * 2);
+        var p = new Vector3(x, y, z);
         transform.position = p;
 
-        transform.Rotate( new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate( new Vector3(30, 30, 30) * Time.deltaTime);
     }
 }
