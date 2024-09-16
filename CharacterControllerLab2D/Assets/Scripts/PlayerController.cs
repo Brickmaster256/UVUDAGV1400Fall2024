e5fabcc7bec5ec76d82a29a36eb3aeb66c9bc1de
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-16 08:06:56 -0600

    added methods to the controller script, they allow the player to move

--- CharacterControllerLab2D/Assets/Scripts/PlayerController.cs
+++ CharacterControllerLab2D/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveCharacter();
+        KeepCharacterOnAxis();
+    }
+
+    private void MoveCharacter()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector *= (moveSpeed * Time.deltaTime);
+        controller.Move(movementVector);
+    }
+
+    private void KeepCharacterOnAxis()
+    {
+        var currentPosition = thisTransform.position;
+        currentPosition.z = 0f;
+        thisTransform.position = currentPosition;
     }
 }
