18d7e4b5849ffcefd4d99467ae300e4801eaf897
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-16 07:54:44 -0600

    created data members for the playerController class

--- CharacterControllerLab2D/Assets/Scripts/PlayerController.cs
+++ CharacterControllerLab2D/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    private CharacterController controller;
+    private Transform thisTransform;
+    private Vector3 movementVector = Vector3.zero;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        thisTransform = transform;
     }
 
     // Update is called once per frame
