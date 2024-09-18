3a4199b858f6a7b753d66b24dea12a20b12ed11d
Matthew Camacho <mcamacho2056@gmail.com>
2024-09-18 09:36:37 -0600

    added a script to the character art to flip it

--- CharacterControllerLab2D/Assets/Scripts/FlipTransformbehavior.cs
+++ CharacterControllerLab2D/Assets/Scripts/FlipTransformbehavior.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FlipTransformbehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public KeyCode key1 = KeyCode.RightArrow, key2 = KeyCode.LeftArrow;
+    public float direction1 = 0, direction2 = 180;
+    
     void Update()
     {
-        
+        if(Input.GetKeyDown(key1))
+        {
+            transform.rotation = Quaternion.Euler(0, direction1, 0);
+
+        }
+
+        if(!Input.GetKeyDown(key2))
+        {
+            return;
+        }
+        transform.rotation = Quaternion.Euler(0, direction2, 0);
     }
 }
