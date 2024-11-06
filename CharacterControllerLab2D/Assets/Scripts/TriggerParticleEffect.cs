79c1cf7ca360a164bb1f266345f177586e58708f
Matthew Camacho <mcamacho2056@gmail.com>
2024-11-06 15:07:35 -0700

    added an IEnumerator function to created better control over particles

--- CharacterControllerLab2D/Assets/Scripts/TriggerParticleEffect.cs
+++ CharacterControllerLab2D/Assets/Scripts/TriggerParticleEffect.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
     private ParticleSystem particleSystem;
-    public int particleAmount = 10;
+
+    
+    public int firstEmissionAmount = 10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delayBetweenEmissions = 0.5f;
 
     private void Start()
     {
@@ -17,10 +23,22 @@ public class TriggerParticleEffect : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CharacterController>())
         {
-            particleSystem.Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine()); 
         }
 
     }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+
+        particleSystem.Emit(thirdEmissionAmount);
+    }
+
 
 
 }
