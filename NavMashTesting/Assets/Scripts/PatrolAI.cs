f9ba29b0eb80474587820b599664b73036191897
Matthew Camacho <mcamacho2056@gmail.com>
2024-11-20 16:57:15 -0700

    added a method that allows the enemy to switch between patrol mode and chase mode

--- NavMashTesting/Assets/Scripts/PatrolAI.cs
+++ NavMashTesting/Assets/Scripts/PatrolAI.cs
@@ -5,13 +5,22 @@ using UnityEngine.AI;
 
 public class PatrolAI : MonoBehaviour
 {
+
     public Transform[] waypoints;
+    [SerializeField] private float sightDistance = 5f;
+    [SerializeField] private LayerMask playerLayer;
     private int currentWayPointIndex;
     private NavMeshAgent agent;
+
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        
+        
+        
         if (waypoints.Length > 0 )
         {
             agent.SetDestination(waypoints[0].position);
@@ -21,7 +30,29 @@ public class PatrolAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (agent.remainingDistance < agent.stoppingDistance)
+        bool playerInRange = Physics.CheckSphere(transform.position, sightDistance, playerLayer);
+
+        if(playerInRange)
+        {
+            Debug.Log("Attack!");
+            ChaseMode();
+        }
+        else
+        {
+            Debug.Log("Patrolling");
+            PatrolMode();
+        }
+        
+    }
+
+    private void ChaseMode()
+    {
+        
+        agent.SetDestination(player.transform.position);
+    }
+    private void PatrolMode()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
         {
             currentWayPointIndex = (currentWayPointIndex + 1) % waypoints.Length;
             agent.SetDestination(waypoints[currentWayPointIndex].position);
